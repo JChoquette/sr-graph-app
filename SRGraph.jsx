745121e8b2896260f63a3602180573a018745129
jchoquette <jeremie8@msn.com>
2025-04-04 16:00:51 -0400

    Added boilerplates, main header

--- SRGraph.jsx
+++ SRGraph.jsx
@@ -1,3 +1,23 @@
+/**
+ * @file SRGraph.jsx
+ * @copyright Copyright (C) 2025 Jeremie Choquette
+ *
+ * This file is part of the SRGraph project.
+ *
+ * SRGraph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SRGraph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SRGraph.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React, { useState } from "react";
 import Observer from "./Observer.jsx";
 import Event from "./Event.jsx";
@@ -137,123 +157,126 @@ const SRGraph = () => {
     let gamma = Constants.get_gamma(v);
 
     return (
-        <div style={{ display: "flex" }}>
-            {/* Graph Area */}
-            <div>
+        <div>
+            <h2>Lorentz Transformation Simulation</h2>
+            <div style={{ display: "flex" }}>
+                {/* Graph Area */}
                 <div>
-                <svg width={width} height={height} onMouseMove={handleMouseMove}>
-                    {/* Axes */}
-                    <line x1={xScale(-10)} x2={xScale(10)} y1={yScale(0)} y2={yScale(0)} stroke="black" />
-                    <line x1={xScale(0)} x2={xScale(0)} y1={yScale(-10)} y2={yScale(10)} stroke="black" />
-                    {/* Grid */}
-                    {Constants.linspace(-10,10,11).map((number)=>[
-                        <line x1={xScale(-10)} x2={xScale(10)} y1={yScale(number)} y2={yScale(number)} stroke="black" opacity={0.5}/>,
-                        <line x1={xScale(number)} x2={xScale(number)} y1={yScale(-10)} y2={yScale(10)} stroke="black" opacity={0.5}/>
-                    ])}
-                    {/* X Axis Label */}
-                    <text 
-                        x={xScale(10)-30} 
-                        y={yScale(0)-10} 
-                        textAnchor="middle" 
-                        fontSize="22"
-                    >
-                        x/c (s)
-                    </text>
-
-                    {/* Y Axis Label */}
-                    <text 
-                        x={xScale(0)+20} 
-                        y={yScale(10)+20} 
-                        textAnchor="middle" 
-                        fontSize="22"
-                    >
-                        t (s)
-                    </text>
-                    {/* Render each observer */}
-                    {observers.map((observer, index) => (
-                        <Observer key={index} v={v} gamma={gamma} data={observer} xScale={xScale} yScale={yScale} />
-                    ))}
-                    {events.map((event, index) => (
-                        <Event key={index} v={v} gamma={gamma} data={event} xScale={xScale} yScale={yScale} />
-                    ))}
-                    {extendeds.map((extended, index) => (
-                        <Extended key={index} v={v} gamma={gamma} data={extended} xScale={xScale} yScale={yScale} />
-                    ))}
-                    {segments.map((segment, index) => (
-                        <Segment key={index} v={v} gamma={gamma} data={segment} xScale={xScale} yScale={yScale} />
-                    ))}
-                    {/* Current cursor position */}
-                    <g stroke="black" strokeWidth="1">
-                        {/* Vertical line */}
-                        <line x1={xScale(cursorPos.x)} y1={yScale(cursorPos.y) - 5} x2={xScale(cursorPos.x)} y2={yScale(cursorPos.y) + 5} />
-                        {/* Horizontal line */}
-                        <line x1={xScale(cursorPos.x) - 5} y1={yScale(cursorPos.y)} x2={xScale(cursorPos.x) + 5} y2={yScale(cursorPos.y)} />
-                        <text
-                            x={xScale(-10)+10}
-                            y={yScale(-10)-20}
-                            fontsize="18"
+                    <div>
+                    <svg width={width} height={height} onMouseMove={handleMouseMove}>
+                        {/* Axes */}
+                        <line x1={xScale(-10)} x2={xScale(10)} y1={yScale(0)} y2={yScale(0)} stroke="black" />
+                        <line x1={xScale(0)} x2={xScale(0)} y1={yScale(-10)} y2={yScale(10)} stroke="black" />
+                        {/* Grid */}
+                        {Constants.linspace(-10,10,11).map((number)=>[
+                            <line x1={xScale(-10)} x2={xScale(10)} y1={yScale(number)} y2={yScale(number)} stroke="black" opacity={0.5}/>,
+                            <line x1={xScale(number)} x2={xScale(number)} y1={yScale(-10)} y2={yScale(10)} stroke="black" opacity={0.5}/>
+                        ])}
+                        {/* X Axis Label */}
+                        <text 
+                            x={xScale(10)-30} 
+                            y={yScale(0)-10} 
+                            textAnchor="middle" 
+                            fontSize="22"
                         >
-                            Cursor: ({cursorPos.x.toFixed(2)}, {cursorPos.y.toFixed(2)})
+                            x/c (s)
                         </text>
-                    </g>
-                </svg>
+
+                        {/* Y Axis Label */}
+                        <text 
+                            x={xScale(0)+20} 
+                            y={yScale(10)+20} 
+                            textAnchor="middle" 
+                            fontSize="22"
+                        >
+                            t (s)
+                        </text>
+                        {/* Render each observer */}
+                        {observers.map((observer, index) => (
+                            <Observer key={index} v={v} gamma={gamma} data={observer} xScale={xScale} yScale={yScale} />
+                        ))}
+                        {events.map((event, index) => (
+                            <Event key={index} v={v} gamma={gamma} data={event} xScale={xScale} yScale={yScale} />
+                        ))}
+                        {extendeds.map((extended, index) => (
+                            <Extended key={index} v={v} gamma={gamma} data={extended} xScale={xScale} yScale={yScale} />
+                        ))}
+                        {segments.map((segment, index) => (
+                            <Segment key={index} v={v} gamma={gamma} data={segment} xScale={xScale} yScale={yScale} />
+                        ))}
+                        {/* Current cursor position */}
+                        <g stroke="black" strokeWidth="1">
+                            {/* Vertical line */}
+                            <line x1={xScale(cursorPos.x)} y1={yScale(cursorPos.y) - 5} x2={xScale(cursorPos.x)} y2={yScale(cursorPos.y) + 5} />
+                            {/* Horizontal line */}
+                            <line x1={xScale(cursorPos.x) - 5} y1={yScale(cursorPos.y)} x2={xScale(cursorPos.x) + 5} y2={yScale(cursorPos.y)} />
+                            <text
+                                x={xScale(-10)+10}
+                                y={yScale(-10)-20}
+                                fontsize="18"
+                            >
+                                Cursor: ({cursorPos.x.toFixed(2)}, {cursorPos.y.toFixed(2)})
+                            </text>
+                        </g>
+                    </svg>
+                    </div>
+                    <div class="vslider">
+                        <input 
+                            type="range" 
+                            min="-0.99" 
+                            max="0.99" 
+                            step="0.01" 
+                            value={v} 
+                            onChange={(e) => setV(parseFloat(e.target.value))}
+                        />
+                        <div>v = {v}</div>
+                    </div>
+                    <div class="bottom-options">
+                        {/* Slider */}
+                        <button onClick={()=>clearAll()}>Reset</button>
+                    </div>
                 </div>
-                <div class="vslider">
-                    <input 
-                        type="range" 
-                        min="-0.99" 
-                        max="0.99" 
-                        step="0.01" 
-                        value={v} 
-                        onChange={(e) => setV(parseFloat(e.target.value))}
-                    />
-                    <div>v = {v}</div>
-                </div>
-                <div class="bottom-options">
-                    {/* Slider */}
-                    <button onClick={()=>clearAll()}>Reset</button>
-                </div>
-            </div>
-            <div>
-                 {/* Observer Controls */}
-                <div style={{ marginLeft: "20px" }}>
-                    <ItemControls 
-                        type={"observer"} 
-                        items={observers} 
-                        addItem={addItem} 
-                        updateItem={updateItem} 
-                        removeItem={removeItem}
-                    />
-                </div>
-                 {/* Event Controls */}
-                <div style={{ marginLeft: "20px" }}>
-                    <ItemControls 
-                        type={"event"} 
-                        items={events} 
-                        addItem={addItem} 
-                        updateItem={updateItem} 
-                        removeItem={removeItem}
-                    />
-                </div>
-                 {/* Extended Object Controls */}
-                <div style={{ marginLeft: "20px" }}>
-                    <ItemControls 
-                        type={"extended"} 
-                        items={extendeds} 
-                        addItem={addItem} 
-                        updateItem={updateItem} 
-                        removeItem={removeItem}
-                    />
-                </div>
-                 {/* Segment Controls */}
-                <div style={{ marginLeft: "20px" }}>
-                    <ItemControls 
-                        type={"segment"} 
-                        items={segments} 
-                        addItem={addItem} 
-                        updateItem={updateItem} 
-                        removeItem={removeItem}
-                    />
+                <div>
+                     {/* Observer Controls */}
+                    <div style={{ marginLeft: "20px" }}>
+                        <ItemControls 
+                            type={"observer"} 
+                            items={observers} 
+                            addItem={addItem} 
+                            updateItem={updateItem} 
+                            removeItem={removeItem}
+                        />
+                    </div>
+                     {/* Event Controls */}
+                    <div style={{ marginLeft: "20px" }}>
+                        <ItemControls 
+                            type={"event"} 
+                            items={events} 
+                            addItem={addItem} 
+                            updateItem={updateItem} 
+                            removeItem={removeItem}
+                        />
+                    </div>
+                     {/* Extended Object Controls */}
+                    <div style={{ marginLeft: "20px" }}>
+                        <ItemControls 
+                            type={"extended"} 
+                            items={extendeds} 
+                            addItem={addItem} 
+                            updateItem={updateItem} 
+                            removeItem={removeItem}
+                        />
+                    </div>
+                     {/* Segment Controls */}
+                    <div style={{ marginLeft: "20px" }}>
+                        <ItemControls 
+                            type={"segment"} 
+                            items={segments} 
+                            addItem={addItem} 
+                            updateItem={updateItem} 
+                            removeItem={removeItem}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
